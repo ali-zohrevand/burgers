8ad65660a90ba3c3e4409ad6621c2da647abd2db
zohrevand.a <zohrevand.alii@gmail.com>
2020-01-28 16:15:01 +0330

    add BurgerBuilder.js component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Layout from './components/Layout/Layout'
 
 function App() {
   return (
     <div className="App">
       سلام.
-        <Layout>test tow</Layout>
+        <Layout>
+            <BurgerBuilder></BurgerBuilder>
+        </Layout>
     </div>
   );
 }
